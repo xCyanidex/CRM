4ca40a8b4ae2832f5e26aec3f64d7d88bea0296a
Mohammad Ryan Chahour <mohammadryanchahour@gmail.com>
2023-12-15 12:41:21 +0500

    created User management

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function productOwner()
+    {
+        return $this->hasOne(ProductOwner::class);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/get-all-users', [UserController::class, 'index']);
+Route::get('/get-user/{id}', [UserController::class, 'show']);
+Route::put('/update-user/{id}', [UserController::class, 'update']);
+Route::delete('/delete-user/{id}', [UserController::class, 'destroy']);
