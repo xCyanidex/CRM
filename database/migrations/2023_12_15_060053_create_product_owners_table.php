ec298e8d5cee4764c689fa7a84eb8fc11fb1a947
marsalan828 <marsalan828@gmail.com>
2023-12-15 16:09:50 +0500

    update

--- database/migrations/2023_12_15_060053_create_product_owners_table.php
+++ database/migrations/2023_12_15_060053_create_product_owners_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
     {
         Schema::create('product_owners', function (Blueprint $table) {
             $table->id();
-            $table->unisgnedBigInteger('user_id')->unique();
+            $table->unsignedBigInteger('user_id')->unique();
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
-        $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+       
     }
 
     /**
