4d1c7d1f76b2f4d34c205d15e79fc911c50caf70
marsalan828 <marsalan828@gmail.com>
2023-12-15 16:29:36 +0500

    migration issue resolved

--- database/migrations/2023_12_15_104538_add_fkto_employees.php
+++ database/migrations/2023_12_15_104538_add_fkto_employees.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('employees',function(Blueprint $table){
+        Schema::table('employees',function(Blueprint $table){
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('company_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
