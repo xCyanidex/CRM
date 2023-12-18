90121ec4422be2d2a83fa165edca885f2d88ba9a
Mohammad Ryan Chahour <mohammadryanchahour@gmail.com>
2023-12-18 12:29:21 +0500

    productOwner api complete

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductOwnerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,12 +21,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+// Routes for Authentication Management
 Route::post('/register', [AuthController::class, 'register']);
-Route::get('/users', [AuthController::class,'getAllUsers']);
+// Route::get('/users', [AuthController::class,'getAllUsers']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/register/company', [AuthController::class, 'registerCompany']);
-Route::get('/get-all-users', [UserController::class, 'index']);
-Route::get('/get-user/{id}', [UserController::class, 'show']);
-Route::put('/update-user/{id}', [UserController::class, 'update']);
-Route::delete('/delete-user/{id}', [UserController::class, 'destroy']);
+
+// Routes for User Management
+Route::get('/get-all-users', [UserController::class, 'getAllUsers']);
+Route::get('/get-user/{id}', [UserController::class, 'getUser']);
+Route::put('/update-user/{id}', [UserController::class, 'updateUser']);
+Route::delete('/delete-user/{id}', [UserController::class, 'deleteUser']);
+
+// Routes for Product Owner Management
+Route::get('/create-product-owner', [ProductController::class, 'store']);
+Route::get('/get-all-product-owners', [ProductOwnerController::class, 'index']);
+Route::get('/get-product-owner/{id}', [ProductOwnerController::class, 'show']);
+Route::put('/update-product-owner/{id}', [ProductOwnerController::class, 'update']);
+Route::delete('/delete-product-owner/{id}', [ProductOwnerController::class, 'destroy']);
