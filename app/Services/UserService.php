164d12010774c25c64a3c5e1abb38ff896cadfba
Mohammad Ryan Chahour <mohammadryanchahour@gmail.com>
2023-12-21 16:50:41 +0500

    update Auth

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -22,59 +22,6 @@ class UserService
         $this->freelancerRepository = $freelancerRepository;
         $this->employeeRepository = $employeeRepository;
     }
-
-    public function register(array $data)
-    {
-        $user = $this->userRepository->createUser([
-            'username' => $data['username'],
-            'email' => $data['email'],
-            'password' => $data['password'],
-            'userType' => $data['userType'],
-        ]);
-    
-        $entityType = $data['userType'];
-    
-        if ($user) {
-            if ($entityType === 'company') {
-                $company = $this->companyRepository->createCompany([
-                    'company_name' => $data['company_name'],
-                    'business_type' => $data['business_type'],
-                    'industry' => $data['industry'],
-                    'registration_number' => $data['registration_number'],
-                    'user_id' => $user->id, // Assigning the user_id to the company
-                ]);
-                if (!$company) {
-                    throw new \Exception('Company creation failed');
-                }
-            } elseif ($entityType === 'freelancer') {
-                $freelancer = $this->freelancerRepository->createFreelancer([
-                    'freelancer_name' => $data['freelancer_name'],
-                    'industry' => $data['industry'],
-                    'user_id' => $user->id, // Assigning the user_id to the freelancer
-                ]);
-                if (!$freelancer) {
-                    throw new \Exception('Freelancer creation failed');
-                }
-            }
-            elseif ($entityType === 'employee') {
-                $employee = $this->employeeRepository->createEmployee([
-                    'employee_name' => $data['employee_name'],
-                    'phone_number' => $data['phone_number'],
-                    'dob' => $data['dob'],
-                    'gender' => $data['gender'],
-                    'user_id' => $user->id, // Assigning the user_id to the freelancer
-                    'department_id' => $data['department_id'],
-                    
-                ]);
-                if (!$employee) {
-                    throw new \Exception('Employee creation failed');
-                }
-            }
-            return $user;
-        } else {
-            throw new \Exception('User creation failed');
-        }
-    }
             
     public function findByEmail($email)
     {
